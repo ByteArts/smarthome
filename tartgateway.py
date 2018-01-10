7d0cbd7bab2a3f72e6748042c53ce24cc0d7f088
Scott Pinkham <ByteArts@users.noreply.github.com>
2018-01-10 09:43:06 -0700

    Added ability to queue up commands to the gateway. Bug fixes.

--- tartgateway.py
+++ tartgateway.py
@@ -14,7 +14,7 @@ import serial_utils
 import time
 import sys
 from event import TEvent
-from threading import Thread
+import threading
 
 #===CONSTANTS=========================================================
 
@@ -23,67 +23,104 @@ SENSOR_TYPE_TEMP = 2
 GW_EVENT_JOIN = 0
 GW_EVENT_SCAN = 1
 
+LOG_LEVEL = 0
+
 #===CLASSES===========================================================
 
-
 class TGatewayManager:
+    """
+    Class for managing communication with a Tarts wireless sensor gateway
+    """
     def __init__(self):
         self.OnSensorData = TEvent()
         self.OnGatewayEvent = TEvent()
         self.SensorList = []
         self.GatewayID = '?'
+        self._cmdlist = []
+        self._cmdlistlock = threading.Lock()
         self._port = None
         self._thread = None
         self._stop = False
+
+
+    def AddCommandToQueue(self, cmd):
+        """
+        Adds a command to the queue. Commands in the queue are sent while the monitor
+        is running.
+        """
+        with self._cmdlistlock:
+            self._cmdlist.append(cmd)
         
 
     def Find(self, ports):
         return FindGateway(ports)
 
 
-    def _MonitorGateway(self, port):
+    def _do_gateway_monitor(self, port):
+        """
+        Communicate with the gateway and monitor it for data. Runs in an infinite loop
+        until _stop is False.
+        """
         # <TODO: check that port is valid and is open>
         port.write('\r')
+        time.sleep(0.5)
+        port.write('at$\r')
+        time.sleep(2)
         port.flushInput()
-        time.sleep(1)
-        port.write('at$id\r') # request ID
-        time.sleep(1)
-        port.write('at$wl\r') # request list of devices
-        
+
+        with self._cmdlistlock:
+            self._cmdlist.append('at$id\r')
+            self._cmdlist.append('at$wl\r')
+                
         # loop, checking for data 
-        print '\nMonitoring gateway data..'
+        LogEntry(2, '\nMonitoring gateway data..')
         while not self._stop:
+            # check for any commands that need to be sent
+            if len(self._cmdlist) > 0:
+                with self._cmdlistlock:
+                    cmd = self._cmdlist[0]
+                    LogEntry(2, 'Sending cmd: {0}'.format(cmd.strip()))
+                    port.write(cmd)
+                    del self._cmdlist[0]
+                time.sleep(1)
+
+            # check for data from gateway
             response = port.readline().lower().strip()
+            
             if (len(response) > 2): # ignore lines with no data
-                #print 'Data recvd:{0}'.format(response),
+                #print '\r\nData recvd:{0}'.format(response),
                 if (response.startswith('id:')): # gateway id
                     self.GatewayID = ParseGatewayID(response)
-                    print 'GatewayID: {0}'.format(self.GatewayID)
+                    LogEntry(2, 'GatewayID: {0}'.format(self.GatewayID))
                 elif (response.startswith('wd:')): # sensor data report
                     data = ParseSensorData(response)
+                    LogEntry(2, 'Sensor data: {0}'.format(data))
                     self.OnSensorData(data) # fire the event
                 #end if
                 elif (response.startswith('wl:')): # sensor list
                     self.SensorList.append(ParseDeviceList(response))
-                    #print 'Sensor list: {0}'.format(self.SensorList)
+                    LogEntry(2, 'Sensor list: {0}'.format(self.SensorList))
                 #end elif
                 elif (response.startswith('wj:')): # sensor joined network
                     data = StrAfter('wj:', response)
-                    #print 'Sensor {0} joined network'.format(data)
+                    LogEntry(2, 'Sensor {0} joined network'.format(data))
                     self.OnGatewayEvent(GW_EVENT_JOIN, data)
                 #end elif
                 elif (response.startswith('ws:')): # sensor scanned network
                     data = ParseScanData(response)
-                    #print 'Sensor {0} scanned network, sensor is {1}'.format(data['id'], data['known'])
+                    LogEntry(2, 'Sensor {0} scanned network, sensor is {1}'.format(data['id'], data['known']))
                     self.OnGatewayEvent(GW_EVENT_SCAN, data)
                 #end elif
             #end if      
         #end while
-    #end def _MonitorGateway        
+    #end def _do_gateway_monitor        
 
 
-    def StartMonitor(self, portname):
-        # <TODO: make sure a thread isn't already running>
+    def Start(self, portname):
+        """
+        Opens the serial port and starts a thread to monitor for data from the gateway. It will also
+        process the command queue and send commands from the queue out to the gateway.
+        """
         if (self._thread <> None):
             return False
         else:
@@ -97,24 +134,27 @@ class TGatewayManager:
 
             # start monitor in background thread
             self._stop = False
-            self._thread = Thread(target=self._MonitorGateway, args=(self._port,))
+            self._thread = threading.Thread(target=self._do_gateway_monitor, args=(self._port,))
             self._thread.start()
             return True
         #end else
     #end def StartMonitor()
 
-    def StopMonitor(self):
-        # <TODO: make sure thread is running>
-        print 'stopping monitor..'
 
-        # set flag to signal to thread to exit
-        self._stop = True
+    def Stop(self):
+        """
+        """
+        if (self._thread <> None):
+            LogEntry(1, 'stopping monitor..')
 
-        # wait for thread to complete
-        self._thread.join()
+            # set flag to signal to thread to exit
+            self._stop = True
 
-        print 'thread complete'
-        self._thread = None
+            # wait for thread to complete
+            self._thread.join()
+
+            LogEntry(1, 'thread complete')
+            self._thread = None
 
         # close serial port
         if (self._port <> None):
@@ -126,6 +166,12 @@ class TGatewayManager:
         
 
 #===METHODS===========================================================
+
+def LogEntry(level, value):
+    global LOG_LEVEL
+    if level <= LOG_LEVEL:
+        sys.stdout.write('gw:' + value + '\n') #SLog(value)
+#end def LogEntry()    
 
 
 #=====================================================================
@@ -234,9 +280,9 @@ def FindGateway(SerialPorts):
     # try to open each port and check for presence of Tarts gateway
     result = ''
     
-    print '\nSearching for gateway on serial ports:'
+    LogEntry(1, '\nSearching for gateway on serial ports:')
     for pn in SerialPorts:
-        print '\tChecking {0}..'.format(pn)
+        LogEntry(2, '\tChecking {0}..'.format(pn))
         port = serial_utils.OpenSerialPort(pn, 9600)
         if (port == None):
             continue
@@ -244,16 +290,16 @@ def FindGateway(SerialPorts):
         try:
             # port is open, try sending a command and checking for a response
             port.write('\rat$\r')
-            time.sleep(0.5)
+            time.sleep(2)
             response = port.read(size=64).lower().strip()
             #print 'response=' + response
 
             if (response.find('ok') <> -1):
-                print 'gateway found'
+                LogEntry(1, 'gateway found')
                 result = pn
                 break
             else:
-                print 'gateway NOT found'
+                LogEntry(2, 'gateway NOT found')
                 continue
             
         except serial.SerialException:
@@ -264,7 +310,7 @@ def FindGateway(SerialPorts):
     #end for p..
 
     if (port == None):
-        print 'Error: no gateway found'
+        LogEntry(1, 'Error: no gateway found')
     else:
         port.close()
 
@@ -272,52 +318,7 @@ def FindGateway(SerialPorts):
 #end def FindGateway()
 
 #=====================================================================
-"""
-Loop forever checking for sensor data. SerPort = serial port (Serial object) that the gateway
-is connected to.
-"""
-"""
-def MonitorGateway(SerPort):
-    SensorList = []
-    GatewayID = '?'
-    
-    SerPort.write('\r')
-    SerPort.flushInput()
-    time.sleep(1)
-    SerPort.write('at$id\r') # request ID
-    time.sleep(1)
-    SerPort.write('at$wl\r') # request list of devices
-    
-    # loop, checking for data
-    print '\nMonitoring gateway data..'
-    while True:
-        response = SerPort.readline().lower().strip()
-        if (len(response) > 2): # ignore lines with no data
-            #print 'Data recvd:{0}'.format(response),
-            if (response.startswith('id:')): # gateway id
-                GatewayID = ParseGatewayID(response)
-                print 'GatewayID: {0}'.format(GatewayID)
-            elif (response.startswith('wd:')): # sensor data report
-                data = ParseSensorData(response)
-                print 'Sensor Data: Time={0} ID={1} Value={2} RSSI={3} Vbat={4}'.format( \
-                data['time'], data['id'], data['value'], data['rssi'], data['vbatt'])
-            #end if
-            elif (response.startswith('wl:')): # sensor list
-                SensorList.append(ParseDeviceList(response))
-                print 'Sensor list: {0}'.format(SensorList)
-            #end elif
-            elif (response.startswith('wj:')): # sensor joined network
-                data = StrAfter('wj:', response)
-                print 'Sensor {0} joined network'.format(data)
-            #end elif
-            elif (response.startswith('ws:')): # sensor scanned network
-                data = ParseScanData(response)
-                print 'Sensor {0} scanned network, sensor is {1}'.format(data['id'], data['known'])
-            #end elif
-        #end if      
-    #end while
-#end def MonitorGateway
-"""
+
 
 #===MAIN PROGRAM==================================================================
 
@@ -326,46 +327,53 @@ Event handlers that are called when certain events occur on the gateway
 """
 
 def GatewayEvent(kind, data):
-    print 'GatewayEvent: kind={0} data={1}'.format(kind, data)
+    LogEntry(1, 'GatewayEvent: kind={0} data={1}'.format(kind, data))
 #end def GatewayEvent()
 
 
 def SensorDataEvent(data):
-    print 'SensorDataEvent: data={0}'.format(data)
+    LogEntry(1, 'SensorDataEvent: data={0}'.format(data))
 #end def SensorDataEvent
 
 
-"""
-Check for serial ports, then look for the gateway and start monitoring data
-"""
-
-# get list of available serial ports
-print 'Enumerating serial ports..',
-aSerialPorts = serial_utils.GetSerialPortNames()
-print '{0} serial port(s) found:'.format(len(aSerialPorts))
-for p in aSerialPorts:
-    print '\t{0}'.format(p)
-
-if (len(aSerialPorts) < 1):
-    print 'Error: no serial ports found'
-    sys.exit(-1)
-#end if
-
-GatewayMgr = TGatewayManager()
-portname = GatewayMgr.Find(aSerialPorts)
-if (portname == ''):
-    sys.exit(-2)
-
-# assign event handlers
-GatewayMgr.OnSensorData += SensorDataEvent
-GatewayMgr.OnGatewayEvent += GatewayEvent
-
-# start monitoring the gateway
-print 'GatewayMgr running. Type "GatewayMgr.StopMonitor()" to stop'
-GatewayMgr.StartMonitor(portname)
-
-#while (True):
-    # check for a keypress
+def StartGateway(on_data_event):
+    """
+    Check for serial ports, then look for the gateway and start monitoring data
+    """
     
+    # get list of available serial ports
+    LogEntry(1, 'Enumerating serial ports..')
+    ports = serial_utils.GetSerialPortNames()
+    LogEntry(1, '{0} serial port(s) found:'.format(len(ports)))
+    for p in ports:
+        LogEntry(1, '\t{0}'.format(p))
 
+    if (len(ports) < 1):
+        LogEntry(0, 'Error: no serial ports found')
+        return None
+    #end if
+
+    gateway = TGatewayManager()
+    portname = gateway.Find(ports)
+    if (portname == ''):
+        return None
+
+    # assign event handlers
+    gateway.OnSensorData += on_data_event
+    #gateway.OnGatewayEvent += GatewayEvent
+
+    # start monitoring the gateway
+    LogEntry(0, 'Starting gateway..')
+    gateway.Start(portname)
+    #print 'Gateway running. Type "Gateway.Stop()" to stop'
+    return gateway
+#end def StartGateway()
+
+gw = None
+
+def Test():
+    global gw
+    print 'Starting test..'
+    gw = StartGateway(SensorDataEvent)
+    print 'Type gw.Stop() to end'
 #end
